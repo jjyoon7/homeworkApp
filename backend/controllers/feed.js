d665cd916aa42302c93640a536a5aea0a79c50ee
jjyoon7 <jay.yoon7@gmail.com>
2020-06-15 16:51:14 +0200

    add errors in controller, to return status 422 and error message when user input data validation has failed.

--- backend/controllers/feed.js
+++ backend/controllers/feed.js
@@ -1,3 +1,5 @@
+const validationResult = require('express-validator/check')
+
 exports.getPosts = (req, res, next) => {
     res.status(200).json({
         posts: [{ 
@@ -14,6 +16,13 @@ exports.getPosts = (req, res, next) => {
 }
 
 exports.createPost = (req, res, next) => {
+    const errors = validationResult(req)
+    if(!errors.isEmpty()){
+        return res.status(422).json({
+            message: 'Validation failed, user input data is incorrect.',
+            errors: errors.array()
+        })
+    }
     console.log('POST')
     const title = req.body.title
     const content = req.body.content
