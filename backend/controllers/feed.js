1766097ab1752bab7b3738a9a15cedd16ef38df4
jjyoon7 <jay.yoon7@gmail.com>
2020-06-15 20:16:52 +0200

    create new route and controller method to fetch single post

--- backend/controllers/feed.js
+++ backend/controllers/feed.js
@@ -49,3 +49,25 @@ exports.createPost = (req, res, next) => {
             next(err)
         })
 }
+
+exports.getPost = (req, res, next) => {
+    const postId = req.params.postId
+    Post.findById(postId)
+        .then(post => {
+            if(!post) {
+                const error = new Error('Post not found')
+                error.statusCode = 404
+                throw error
+            }
+            res.status(200).json({
+                message: 'Post fetched',
+                post: post
+            })
+        })
+        .catch(err => {
+            if(!err.statusCode){
+                err.statusCode = 500
+            }
+            next(err)
+        })
+}
--- backend/routes/feed.js
+++ backend/routes/feed.js
@@ -4,9 +4,12 @@ const { body } = require('express-validator')
 const feedController = require('../controllers/feed')
 
 router.get('/posts', feedController.getPosts)
+
 router.post('/post', [
     body('title').trim().isLength({min: 5}),
     body('content').trim().isLength({min: 5})
 ], feedController.createPost)
+
+router.get('/post/:postId', feedController.getPost)
 
 module.exports = router
