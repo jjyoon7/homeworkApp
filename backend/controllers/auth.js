e6bbf9647e722cb1cf48fcf72431c2dc33be212e
jjyoon7 <jay.yoon7@gmail.com>
2020-06-16 20:18:27 +0200

    use validationResult(req) to throw en error if there is an error from incoming request

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,6 +1,15 @@
+const { validationResult } = require('express-validator')
 const User = require('../models/user')
 
 exports.signup = (req, res, next) => {
+    const errors = validationResult(req)
+    if(!errors.isEmpty()) {
+        const error = new Error('Validation failed.')
+        error.statusCode = 422
+        error.data = errors.array()
+        throw error
+    }
+    
     const email = req.body.email
     const password = req.body.password
     const name = req.body.name
