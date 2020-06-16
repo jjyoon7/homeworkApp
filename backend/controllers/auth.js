2ae7a4ee7815b28f52359ad18003af61736c0c1e
jjyoon7 <jay.yoon7@gmail.com>
2020-06-16 20:31:38 +0200

    use bcyrpt to hash the password and create a new User with hasedPw then save and return the result

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,4 +1,5 @@
 const { validationResult } = require('express-validator')
+const bcrypt = require('bcryptjs')
 const User = require('../models/user')
 
 exports.signup = (req, res, next) => {
@@ -9,8 +10,30 @@ exports.signup = (req, res, next) => {
         error.data = errors.array()
         throw error
     }
-    
+
     const email = req.body.email
     const password = req.body.password
     const name = req.body.name
+
+    bcrypt.hash(password, 12)
+          .then(hashedPw => {
+              const user = new User({
+                  email: email,
+                  password: hashedPw,
+                  name: name
+              })
+              return user.save()
+          })
+          .then(result => {
+              res.status(201).json({
+                  message: 'New user is created.',
+                  userId: result._id
+              })
+          })
+          .catch(err => {
+            if(!err.statusCode){
+                err.statusCode = 500
+            }
+            next(err)
+          })
 }
