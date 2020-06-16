85dd56d22df612b890534b30449da839aa792386
jjyoon7 <jay.yoon7@gmail.com>
2020-06-16 20:25:29 +0200

    add bcryptjs and update user model where 'status' is not required but has default

--- backend/models/user.js
+++ backend/models/user.js
@@ -5,7 +5,7 @@ const userSchema = new Schema({
     email: { type: String, required: true },
     password: { type: String, required: true },
     name: { type: String, required: true },
-    status: { type: String, required: true },
+    status: { type: String, default: 'New user' },
     posts: [{
         type: Schema.Types.ObjectId,
         ref: 'Post'
