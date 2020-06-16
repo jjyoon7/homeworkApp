cf30a14509bd10b3abed8f0942b169d4b54183e9
jjyoon7 <jay.yoon7@gmail.com>
2020-06-16 20:00:08 +0200

    validate the user input data on signup

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,21 @@
 const express = require('express')
 const router = express.Router()
+const { body } = require('express-validator')
+const User = require('../models/user')
 
-router.put('/auth')
+router.put('/auth', [
+    body('email')
+        .isEmail()
+        .withMessage('Please enter a valid email address')
+        .custom((value, {req}) => {
+            return User.findOne({ email: value })
+                       .then(userData => {
+                           if(userData) {
+                               return Promise.reject('Email address already exists.')
+                           }
+                       })
+        })
+        .normalizeEmail(),
+])
 
 modeul.exports = router
