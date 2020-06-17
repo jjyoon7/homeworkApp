cbaee94b51db87ed7cbe0e898480d445f7686e21
jjyoon7 <jay.yoon7@gmail.com>
2020-06-17 19:34:39 +0200

    create another if block where the try block succedded but the value could be 'undefined', in that case, throw an error 401. If not, save the decodedToken.userId as req.userId and add next() since it is a custom middleware

--- backend/middleware/is-auth.js
+++ backend/middleware/is-auth.js
@@ -3,11 +3,18 @@ const jwt = require('jsonwebtoken')
 module.exports = (req, res, next) => {
 
     const token = req.get('Authorization').split(' ')[1]
-    let decodedtoken
+    let decodedToken
     try {
         decodedToken = jwt.verify(token, 'privatekeytogenerateatokenstring')
     } catch (err) {
         err.statusCode = 500
         throw err
     }
+    if(!decodedtoken) {
+        const error = new Error('Failed authentication.')
+        error.statusCode = 401
+        throw error
+    }
+    req.userId = decodedToken.userId
+    next()
 }
