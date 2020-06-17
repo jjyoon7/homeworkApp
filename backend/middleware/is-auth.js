53f43df796bbd62c46220d92c7a92cd3413ca5b1
jjyoon7 <jay.yoon7@gmail.com>
2020-06-17 19:30:56 +0200

    catch block- will be catched by express error handler since it is a middleware

--- backend/middleware/is-auth.js
+++ backend/middleware/is-auth.js
@@ -1,10 +1,13 @@
 const jwt = require('jsonwebtoken')
 
 module.exports = (req, res, next) => {
-    
+
     const token = req.get('Authorization').split(' ')[1]
     let decodedtoken
     try {
         decodedToken = jwt.verify(token, 'privatekeytogenerateatokenstring')
+    } catch (err) {
+        err.statusCode = 500
+        throw err
     }
 }
