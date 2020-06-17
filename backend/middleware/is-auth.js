007b2e4434c2de5ccf34237a6b057d1731a310b5
jjyoon7 <jay.yoon7@gmail.com>
2020-06-17 19:28:41 +0200

    extract the token from the req headers, using 'Authorization' and decode and verify by using jwt.verify() method

--- backend/middleware/is-auth.js
+++ backend/middleware/is-auth.js
@@ -1,5 +1,9 @@
 const jwt = require('jsonwebtoken')
 
 module.exports = (req, res, next) => {
-    
+    const token = req.get('Authorization').split(' ')[1]
+    let decodedtoken
+    try {
+        decodedToken = jwt.verify(token, 'privatekeytogenerateatokenstring')
+    }
 }
