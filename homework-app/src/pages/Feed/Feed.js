863b55e22ee1a039e2f4e9a4f881fe4cd930d55a
jjyoon7 <jay.yoon7@gmail.com>
2020-06-17 20:11:59 +0200

    add token with headers Authorization in different functions where its fetcehd routes has isAuth as middleware

--- homework-app/src/pages/Feed/Feed.js
+++ homework-app/src/pages/Feed/Feed.js
@@ -130,7 +130,10 @@ class Feed extends Component {
 
     fetch(url, {
       method: method, 
-      body: formData
+      body: formData, 
+      headers: {
+        Authorization:  'Bearer ' + this.props.token
+      }
       })
       .then(res => {
         if (res.status !== 200 && res.status !== 201) {
@@ -182,7 +185,10 @@ class Feed extends Component {
   deletePostHandler = postId => {
     this.setState({ postsLoading: true });
     fetch('http://localhost:5000/feed/post/' + postId, {
-      method: 'DELETE'
+      method: 'DELETE',
+      headers: {
+        Authorization: 'Bearer ' + this.props.token 
+      }
     })
       .then(res => {
         if (res.status !== 200 && res.status !== 201) {
