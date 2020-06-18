3e2bf13248078276924edf865b452143b2d4f4d6
jjyoon7 <jay.yoon7@gmail.com>
2020-06-18 19:33:40 +0200

    getPost update with async/await

--- backend/controllers/feed.js
+++ backend/controllers/feed.js
@@ -74,26 +74,28 @@ exports.createPost = async (req, res, next) => {
     }
 }
 
-exports.getPost = (req, res, next) => {
+exports.getPost = async (req, res, next) => {
     const postId = req.params.postId
-    Post.findById(postId)
-        .then(post => {
-            if(!post) {
-                const error = new Error('Post not found')
-                error.statusCode = 404
-                throw error
-            }
-            res.status(200).json({
-                message: 'Post fetched',
-                post: post
-            })
+    
+    try {
+        const post = await Post.findById(postId)
+
+        if(!post) {
+            const error = new Error('Post not found')
+            error.statusCode = 404
+            throw error
+        }
+
+        res.status(200).json({
+            message: 'Post fetched',
+            post: post
         })
-        .catch(err => {
-            if(!err.statusCode){
-                err.statusCode = 500
-            }
-            next(err)
-        })
+    } catch (err) {
+        if(!err.statusCode){
+            err.statusCode = 500
+        }
+        next(err)
+    }
 }
 
 exports.updatePost = (req, res, next) => {
