b93386e3257d388ab63b2c69f2c950488f073185
jjyoon7 <jay.yoon7@gmail.com>
2020-06-18 19:43:15 +0200

    update login method with async/await

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -40,49 +40,48 @@ exports.signup = async (req, res, next) => {
     }
 }
 
-exports.login = (req, res, next) => {
+exports.login = async (req, res, next) => {
     const email = req.body.email
     const password = req.body.password
-    let loadedUser
+    
+    try {
+        const user = await User.findOne({ email: email })
 
-    User.findOne({ email: email })
-        .then(user => {
-            if(!user) {
-                const error = new Error('User with this email does not exists.')
-                //401 status code means lack validation / unauthorized
-                error.statusCode = 401
-                throw error
-            }
-            loadedUser = user
-            return bcrypt.compare(password, user.password)
-        })
-        .then(isEqual => {
-            if(!isEqual) {
-                const error = new Error('Wrong password')
-                error.statusCode = 401
-                throw error
-            }
-            const token = jwt.sign(
-                    {
-                        email: loadedUser.email,
-                        userId: loadedUser._id.toString()
-                    },
-                    'privatekeytogenerateatokenstring',
-                    { expiresIn: '1h' }
-                )
-            
-            res.status(200).json({
-                token: token,
-                userId: loadedUser._id.toString()
-            })
+        if(!user) {
+            const error = new Error('User with this email does not exists.')
+            //401 status code means lack validation / unauthorized
+            error.statusCode = 401
+            throw error
+        }
+    
+        const isEqual = await bcrypt.compare(password, user.password)
+    
+        if(!isEqual) {
+            const error = new Error('Wrong password')
+            error.statusCode = 401
+            throw error
+        }
 
+        const token = jwt.sign(
+                {
+                    email: user.email,
+                    userId: user._id.toString()
+                },
+                'privatekeytogenerateatokenstring',
+                { expiresIn: '1h' }
+            )
+        
+        res.status(200).json({
+            token: token,
+            userId: user._id.toString()
         })
-        .catch(err => {
-            if(!err.statusCode){
-                err.statusCode = 500
-            }
-            next(err)
-        })
+    
+    } catch (err) {
+        if(!err.statusCode){
+            err.statusCode = 500
+        }
+        next(err)
+    }
 }
 
 exports.getUserStatus = (req, res, next) => {
