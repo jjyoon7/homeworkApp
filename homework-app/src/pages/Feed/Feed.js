fda90bbf95b6964062d5a5f343cfb2028886ff0a
jjyoon7 <jay.yoon7@gmail.com>
2020-06-22 20:23:12 +0200

    update finishHandler function, else if statement was causing duplicate posts for the creator

--- homework-app/src/pages/Feed/Feed.js
+++ homework-app/src/pages/Feed/Feed.js
@@ -192,8 +192,6 @@ class Feed extends Component {
               p => p._id === prevState.editPost._id
             );
             updatedPosts[postIndex] = post;
-          } else if (prevState.posts.length < 2) {
-            updatedPosts = prevState.posts.concat(post);
           }
           return {
             posts: updatedPosts,
