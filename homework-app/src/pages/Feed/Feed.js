a62b8b8dcdad55bf35074ab250ced68a19d85ffa
jjyoon7 <jay.yoon7@gmail.com>
2020-06-22 20:12:05 +0200

    to listen to the initiated IO instance from the frontend, save openSocket('url') as 'socket' variable then use '.on()' method to fetch the IO variable. 'posts' is the channel you created in the backend with emit(), so it should be the same naming. Also, action and post is coming from the emit() as well

--- homework-app/src/pages/Feed/Feed.js
+++ homework-app/src/pages/Feed/Feed.js
@@ -40,7 +40,12 @@ class Feed extends Component {
       .catch(this.catchError);
 
     this.loadPosts();
-    openSocket('http://localhost:5000')
+    const socket = openSocket('http://localhost:5000')
+    socket.on('posts', data => {
+      if(data.action === 'create') {
+        this.addPost(data.post)
+      }
+    })
   }
 
   addPost = post => {
