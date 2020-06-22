7025fadbbf745a28a8c0575259072f77f60573c2
jjyoon7 <jay.yoon7@gmail.com>
2020-06-22 20:50:09 +0200

    create 'updatePost' function in Feed component, update the selected post with updated data

--- homework-app/src/pages/Feed/Feed.js
+++ homework-app/src/pages/Feed/Feed.js
@@ -63,6 +63,19 @@ class Feed extends Component {
       };
     });
   }
+
+  updatePost = post => {
+    this.setState(prevState => {
+      const updatedPosts = [...prevState.posts];
+      const updatedPostIndex = updatedPosts.findIndex(p => p._id === post._id);
+      if( updatedPostIndex > -1) {
+        updatedPosts[updatedPostIndex] = post
+      }
+      return {
+        posts: updatedPosts
+      }
+    })
+  }
   
   loadPosts = direction => {
     if (direction) {
