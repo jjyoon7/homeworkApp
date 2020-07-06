1efe850329b10ae2f6084d409c7cd16120e6ee4e
jjyoon7 <jay.yoon7@gmail.com>
2020-07-06 19:41:15 +0200

    create a dummy 'req' object with method 'get' to test

--- backend/test/auth-middleware.js
+++ backend/test/auth-middleware.js
@@ -1,5 +1,17 @@
 const authMiddleware = require('../middleware/is-auth')
 
 it('should throw an error if there is no authorization header.', function() {
-    
+    //scenario is to check if the req object has authorization header
+
+    //const authHeader = req.get('Authorization')
+    //if above line returns false, it should throw an error.
+    //that is why dummy 'req' object has an method 'get' that returns null
+
+    const req = {
+        get: function() {
+            return null
+        }
+    }
+
+    authMiddleware(req)
 })
