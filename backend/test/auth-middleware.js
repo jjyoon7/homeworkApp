93fbe009a51c52da8255623298343c384e799ba6
jjyoon7 <jay.yoon7@gmail.com>
2020-07-06 20:32:47 +0200

    Organize the test units by using 'describe()'. It takes header string as 1st arg and function as 2nd. You can nest the describe(), to make it detailed header

--- backend/test/auth-middleware.js
+++ backend/test/auth-middleware.js
@@ -2,36 +2,42 @@ const expect = require('chai').expect
 
 const authMiddleware = require('../middleware/is-auth')
 
-it('should throw an error if there is no authorization header.', function() {
-    //scenario is to check if the req object has authorization header
+describe('Auth middleware', function() {
+    describe('Auth Header', function() {
+        it('should throw an error if there is no authorization header.', function() {
+            //scenario is to check if the req object has authorization header
+        
+            //const authHeader = req.get('Authorization')
+            //if above line returns false, it should throw an error.
+            //that is why dummy 'req' object has an method 'get' that returns null
+        
+            const req = {
+                get: function() {
+                    return null
+                }
+            }
+        
+            //authMiddleware takes 3 args (req, res, next)
+            //req, which is what we are testing and created a dummy data for.
+            //{} is res
+            //empty arrow function is next()
+        
+            //The bind() method creates a new function that, 
+            //when called, has its this keyword set to the provided value, 
+            //with a given sequence of arguments preceding any provided when the new function is called.
+            expect(authMiddleware.bind(this, req, {}, () => {})).to.throw('Header: Authorization failed.')
+        })
+    })
 
-    //const authHeader = req.get('Authorization')
-    //if above line returns false, it should throw an error.
-    //that is why dummy 'req' object has an method 'get' that returns null
-
-    const req = {
-        get: function() {
-            return null
-        }
-    }
-
-    //authMiddleware takes 3 args (req, res, next)
-    //req, which is what we are testing and created a dummy data for.
-    //{} is res
-    //empty arrow function is next()
-
-    //The bind() method creates a new function that, 
-    //when called, has its this keyword set to the provided value, 
-    //with a given sequence of arguments preceding any provided when the new function is called.
-    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw('Header: Authorization failed.')
-})
-
-it('should throw an error if there is only one string', function() {
-    const req= {
-        get: function() {
-            return 'string'
-        }
-    }
-
-    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw()
+    describe('Token Existence', function() {
+        it('should throw an error if there is only one string', function() {
+            const req= {
+                get: function() {
+                    return 'string'
+                }
+            }
+        
+            expect(authMiddleware.bind(this, req, {}, () => {})).to.throw()
+        })
+    })
 })
