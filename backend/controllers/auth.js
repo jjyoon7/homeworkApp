ea810ac11ec043189967a2c790ffa09f65936037
jjyoon7 <jay.yoon7@gmail.com>
2020-07-07 19:52:24 +0200

    for integration test where you connect to the real test database instead of using stub, make sure to change the 'message' in uri to 'test-uri'

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -31,14 +31,11 @@ exports.signup = async (req, res, next) => {
             message: 'New user is created.',
             userId: result._id
         })
-        return
-
     } catch (err) {
         if(!err.statusCode){
             err.statusCode = 500
         }
         next(err)
-        return err
     }
 }
 
@@ -77,12 +74,13 @@ exports.login = async (req, res, next) => {
             token: token,
             userId: user._id.toString()
         })
-    
+        return
     } catch (err) {
         if(!err.statusCode){
             err.statusCode = 500
         }
         next(err)
+        return err
     }
 }
 
--- backend/test/auth-controller.js
+++ backend/test/auth-controller.js
@@ -4,6 +4,9 @@ const sinon = require('sinon')
 const User = require('../models/user')
 const AuthController = require('../controllers/auth')
 
+const mongoose = require('mongoose')
+const uri = process.env.ATLAS_TEST_URI
+
 describe('Auth controller', function() {
     describe('Login', function() {
         it('should throw an error code 500 if the access to the database failed', function(done) {
@@ -26,4 +29,25 @@ describe('Auth controller', function() {
             User.findOne.restore()
         })
     })
+
+    describe('getUserStatus', function() {
+        it('should send a response with valid user status for an existing user', function(done) {
+            mongoose.connect(uri, {
+                            useNewUrlParser: true,
+                            useCreateIndex: true,
+                            useUnifiedTopology: true,
+                            useFindAndModify: true
+                            })
+            .then(result => {
+                console.log(`server is running on port ${PORT}`)
+                console.log('mongoDB database connection established successfully.')
+                const server = app.listen(PORT)
+                const io = require('./socket').init(server)
+                io.on('connection', socket => {
+                    console.log('Client connected')
+                })
+            })
+            .catch(err => console.log(err))
+                    })
+                })
 })
