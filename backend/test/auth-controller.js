6237d60e31f89c92e6304dc260700b4fe3b000ae
jjyoon7 <jay.yoon7@gmail.com>
2020-07-07 19:35:31 +0200

    use 'done' argument to make sure the asynchronous function has been executed and finished

--- backend/test/auth-controller.js
+++ backend/test/auth-controller.js
@@ -6,7 +6,7 @@ const AuthController = require('../controllers/auth')
 
 describe('Auth controller', function() {
     describe('Login', function() {
-        it('should throw an error code 500 if the access to the database failed', function() {
+        it('should throw an error code 500 if the access to the database failed', function(done) {
             sinon.stub(User, 'findOne')
             User.findOne.throws()
 
@@ -18,10 +18,12 @@ describe('Auth controller', function() {
             }
 
             AuthController.login(req, {}, () => {}).then(result => {
-                console.log(result)
-            })
+                expect(result).to.be.an('error')
+                expect(result).to.have.property('statusCode', 500)
+                done()
+            }).catch(done)
 
-            sinon.restore()
+            User.findOne.restore()
         })
     })
 })
