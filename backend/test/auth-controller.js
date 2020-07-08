55cbe703129b48762f385d1f58ccd3122393ca72
jjyoon7 <jay.yoon7@gmail.com>
2020-07-08 10:37:28 +0200

    initialize connection to the mongoDB in before, and clean up and disconnect in after

--- backend/test/auth-controller.js
+++ backend/test/auth-controller.js
@@ -12,7 +12,25 @@ const uri = process.env.ATLAS_TEST_URI
 describe('Auth controller', function() {
 
     before(function(done) {
-
+        mongoose.connect(uri, {
+                    useNewUrlParser: true,
+                    useCreateIndex: true,
+                    useUnifiedTopology: true,
+                    useFindAndModify: true
+                    })
+        .then(result => {
+            const user = new User({
+                email: 'test@test.com',
+                password: 'test',
+                name: 'john',
+                posts: [],
+                _id: '5c0f66b979af55031b34729a'
+            })
+            return user.save()
+            })
+        .then(() => {
+            done()
+        })
     })
 
     describe('Login', function() {
@@ -39,57 +57,36 @@ describe('Auth controller', function() {
 
     describe('getUserStatus', function() {
         it('should send a response with valid user status for an existing user', function(done) {
-            mongoose.connect(uri, {
-                            useNewUrlParser: true,
-                            useCreateIndex: true,
-                            useUnifiedTopology: true,
-                            useFindAndModify: true
-                            })
-            .then(result => {
-                const user = new User({
-                    email: 'test@test.com',
-                    password: 'test',
-                    name: 'john',
-                    posts: [],
-                    _id: '5c0f66b979af55031b34729a'
-                })
-
-                return user.save()
-            })
-            .then(() => {
-                const req = {
-                    userId: '5c0f66b979af55031b34729a'
+            
+            const req = {
+                userId: '5c0f66b979af55031b34729a'
+            }
+            //mimik this 'res.status(200).json({status: user.status})'
+            const res = {
+                statusCode: 500,
+                userStatus: null,
+                status: function(code) {
+                    this.statusCode = code
+                    return this
+                },
+                json: function(data) {
+                    this.userStatus = data.status
                 }
-
-                //mimik this 'res.status(200).json({status: user.status})'
-                const res = {
-                    statusCode: 500,
-                    userStatus: null,
-                    status: function(code) {
-                        this.statusCode = code
-                        return this
-                    },
-                    json: function(data) {
-                        this.userStatus = data.status
-                    }
-                }
-
-                AuthController.getUserStatus(req, res, () => {}).then(() => {
-                    expect(res.statusCode).to.be.equal(200)
-                    expect(res.userStatus).to.be.equal('New user')
-                    User.deleteMany({})
-                        .then(() => {
-                            return mongoose.disconnect()
-                        }).then(()=>{
-                            done()
-                        })
-                }).catch(done)
+            }
+            AuthController.getUserStatus(req, res, () => {}).then(() => {
+                expect(res.statusCode).to.be.equal(200)
+                expect(res.userStatus).to.be.equal('New user')
+                done()
             })
-            .catch(err => console.log(err))
-                    })
+
     })
 
     after(function(done) {
-        
+        User.deleteMany({})
+        .then(() => {
+            return mongoose.disconnect()
+        }).then(()=>{
+            done()
+        })
     })
 })
